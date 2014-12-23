d488e02b88debe9e1a027b2a067de17e5749320f
Ilya <bound1ess.dev@gmail.com>
2014-12-23 21:53:27 +0600

    Add the installation script.

--- src/install.sh
+++ src/install.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 echo "Installing Git..."
+
+# Check if Git is already installed on the machine
+if [ "$(git --version > /dev/null 2>&1)" != 0 ]; then 
+
+	echo "OK, performing installation..."
+
+	# Install Git with Apt
+	sudo apt-get update
+	sudo apt-get install git -y
+
+else
+
+	# You already have it installed, come on then!
+	echo "Git is already installed on your machine, as I can see."
+	git --version	
+
+fi
--- test/installation.sh
+++ test/installation.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Testing Git installation..."
+
+if [ "$(git --version > /dev/null 2>&1)" == 0 ]; then 
+
+	echo "Looks like Git was successfully installed!"
+	git --version
+
+else
+
+	echo "Something went wrong and Git (probably) was not installed properly."
+	
+fi
