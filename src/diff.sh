e3dc6e9f69d5d46b0cb857b4c96fc87c54fa8f44
vagrant <vagrant@vagrant.com>
2014-12-24 20:28:15 +0600

    Work on diff.sh.

--- src/diff.sh
+++ src/diff.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
 echo "Configuring an external diff viewer..."
+
+PY_SCRIPT="~/external_diff_viewer.py"
+
+TEMPLATE="
+#!/usr/bin/env python3
+from subprocess import Popen
+from sys import argv
+
+# By default, it is configured to use Meld as the diff viewer of choice.
+Popen([\"meld\", argv[2], argv[5]], ).wait()
+"
+
+cat "$TEMPLATE" > "$PY_SCRIPT"
+git config --global diff.external "$PY_SCRIPT"
+
+echo "Now you can edit $PY_SCRIPT to add support for your favorite diff viewer."
--- test/diff.sh
+++ test/diff.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Testing external diff viewers..."
+
+PY_SCRIPT="~/external_diff_viewer.py"
+
+if [ -f "$PY_SCRIPT" ] && [ $(git config --get diff.external) == "$PY_SCRIPT" ]; then
+
+	echo "Looks like Git was properly configured to use an external diff viewer!"
+
+else
+
+	echo "Something went terribly wrong - external diff viewer was not added."
+
+fi
