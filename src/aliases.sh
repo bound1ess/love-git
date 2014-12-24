989d84c1585c5013adf640ccab0dbe7c3f4ddc53
vagrant <vagrant@vagrant.com>
2014-12-24 18:54:40 +0600

    Aliases.sh: first draft.

--- src/aliases.sh
+++ src/aliases.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Adding aliases..."
+
+echo "Please specify the shell configuration file path: "
+read CONFIG_PATH
+
+# more to be added
+cat "alias gi=\"git init\"" >> "$CONFIG_PATH"
+cat "alias ga=\"git add\"" >> "$CONFIG_PATH"
+cat "alias gs=\"git status\"" >> "$CONFIG_PATH"
+cat "alias gc=\"git commit\"" >> "$CONFIG_PATH"
+cat "alias gp=\"git push\"" >> "$CONFIG_PATH" 
+
+echo "Aliases were added to $CONFIG_PATH!"
--- test/aliases.sh
+++ test/aliases.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
 echo "Testing added aliases..."
+
+alias_exists() {
+	if [ $(alias | grep "$1=" | wc -l) == 0 ]; then
+		echo "Alias $1 is not set."
+	else
+		echo "Alias $1 is set correctly."
+	fi
+}
+
+alias_exists "gi"
+alias_exists "ga"
+alias_exists "gs"
+alias_exists "gc"
+alias_exists "gp"
